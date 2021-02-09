b9c3dc7f8250c826e876667c42b2b0311f38250a
Atanu <atanupal@live.com>
2021-02-09 16:36:31 +0530

    database connection created

---
+++ conn.php
@@ -0,0 +1,20 @@
+<?php
+
+	$con = mysqli_connect("localhost","root","");
+	$db = mysqli_select_db($con, "shub");
+
+	echo ($db == 1)? "db connected" : "db not connected";
+
+	if(!$con){
+		die("Error: Failed to connect to database!");
+	}
+
+	$query = mysqli_query($con, "select * from admin") or die(mysqli_error());
+	$default = mysqli_num_rows($query);
+	//var_dump($default);
+
+	if($default === 0){
+		$pass = md5('admin');
+		mysqli_query($con, "insert into admin values('','administrator','','admin','$pass','admin')");
+		return false;
+	}
