c146cf35a8591d70425486e4ee66e7e1aef4bca7
Atanu <atanupal@live.com>
2021-03-18 14:35:29 +0530

    Add & Remove file pages added

---
+++ remove_file.php
@@ -0,0 +1,22 @@
+<?php
+	require_once 'conn.php';
+
+	if(isset($_GET['f_id'])){
+		$f_id = $_GET['f_id'];
+		$select = "select * from storage where f_id='$f_id'";
+		$query = mysqli_query($con, $select);
+		$fetch = mysqli_fetch_array($query);
+
+		$filename = $fetch['filename'];
+		$s_id = $fetch['s_id'];
+		$s_name = $fetch['s_name'];
+
+		if(unlink("files/".$s_id."_".$s_name."/".$filename)){
+			$delete = mysqli_query($con, "delete from storage where f_id='$f_id'");
+
+			if($delete == 1){
+				header('location:student_profile.php');
+			}
+		}
+	}
+?>
