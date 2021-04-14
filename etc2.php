8a9717b5bfea8773731487122c8808643742927c
Atanu <atanupal@live.com>
2021-04-14 16:05:49 +0530

    Video call features added

---
+++ etc2.php
@@ -0,0 +1,6 @@
+<?php
+	# student videocall page
+?><body>
+	<div id="otEmbedContainer" style="width:800px; height:640px"></div>
+                    <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=d4126eda-a9bd-493a-b09d-5734d8ca0d50&room=DEFAULT_ROOM"></script>
+</body>
