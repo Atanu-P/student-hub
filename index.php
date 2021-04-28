c1646d330df64bab4c27ba2cb0c0a3d1402453fe
Atanu <atanupal@live.com>
2021-04-28 19:42:32 +0530

    add fontawsome icons on index page

--- index.php
+++ index.php
@@ -18,29 +18,31 @@
     		<h1 class="display-4" align="center">Welcome to</h1>
     		<h1 class="display-1" align="center">Student-HUB</h1>
     		<hr class="my-4">
-    		<p class="lead">It uses utility classes for typography and spacing to space content out within the larger container</p>
+    		<p class="lead"></p>
     	</div>
     	<div class="row">
-    		<div class="col">
+    		<div class="col text-center">
     			<!-- admin or teacher login form-->
 				<form action="login.php" method="POST" class="form-signin">
-					<h1 class="h3 mb-3 font-weight-normal">Adminstrator Log In</h1>
+					<img class="mb-4" src="svgs\solid\portrait.svg" alt="" width="72" height="72" align="center">
+					<h1 class="h3 mb-3 font-weight-normal">Adminstrator Log-In</h1>
 					<label for="" class="sr-only">Username :</label>
 					<input type="Username" name="username" placeholder="Username" class="form-control" required autofocus><br>
 					<label for="" class="sr-only">Password :</label>
 					<input type="Password" name="pass" placeholder="Password" class="form-control" required autofocus><br>
-					<button type="submit" name="submit" class="btn btn-lg btn-primary btn-block">Log In</button>
+					<button type="submit" name="submit" class="btn btn-lg btn-primary btn-block">Login</button>
 				</form>
     		</div>
-    		<div class="col">
+    		<div class="col text-center">
     			<!-- student login form-->
 				<form action="student_login.php" method="post" class="form-signin">
-					<h1 class="h3 mb-3 font-weight-normal">Student Log in</h1>
+					<img class="mb-4" src="svgs\solid\user-friends.svg" alt="" width="72" height="72" align="center">
+					<h1 class="h3 mb-3 font-weight-normal">Student Log-In</h1>
 					<label for="" class="sr-only">Student Id : </label>
-					<input type="number" name="id" placeholder="Enter Id" class="form-control" required autofocus><br>
+					<input type="number" name="id" placeholder="Student Id" class="form-control" required autofocus><br>
 					<label for="" class="sr-only">Password : </label>
-					<input type="password" name="pass" placeholder="Enter Password" class="form-control" required autofocus=""><br>
-					<button type="submit" name="submit" class="btn btn-lg btn-primary btn-block">Log In</button>
+					<input type="password" name="pass" placeholder="Password" class="form-control" required autofocus=""><br>
+					<button type="submit" name="submit" class="btn btn-lg btn-primary btn-block">Login</button>
 				</form>
     		</div>
     	</div>
