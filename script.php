559fce68faf48b3f68b18662724ed7bb9eb386b5
Atanu <atanupal@live.com>
2021-05-07 17:44:14 +0530

    MD Bootstrap CSS & JS added

--- script.php
+++ script.php
@@ -1,7 +1,15 @@
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     
-<script type="text/javascript" src="js\jquery-3.6.0.slim.js"></script>
+<script type="text/javascript" src="js/jquery-3.6.0.slim.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script type="text/javascript" src="js\all.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js" ></script>
+
+<!-- font awsome JavaScript -->
+<script type="text/javascript" src="js/all.js"></script>
+
+<!-- Material Design for Bootstrap -->
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<script type="text/javascript" src="js/datatables.min.js"></script>
--- stylesheet.php
+++ stylesheet.php
@@ -1,3 +1,6 @@
-<link rel="stylesheet" type="text/css" href="css\bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css\all.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/all.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/mdb.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/datatables.min.css">
