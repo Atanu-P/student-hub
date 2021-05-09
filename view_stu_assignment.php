43778d40aae24dc1b16a5378959d0d44aacff60a
Atanu <atanupal@live.com>
2021-05-09 19:35:29 +0530

    Added styling in  view_stu_assignment page

--- view_stu_assignment.php
+++ view_stu_assignment.php
@@ -8,35 +8,105 @@
 	$query = mysqli_query($con, "select * from student where id='$id'");
 	$fetch_detail = mysqli_fetch_array($query);
 ?>
-<h1><?= $fetch_detail['fname'].' '.$fetch_detail['lname']?></h1><hr>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<h2>Id : <?= $fetch_detail['id']?></h2>
+    <!-- Bootstrap CSS -->
+    <?php include "stylesheet.php";?>
+    <style type="text/css">
+		table.dataTable thead .sorting:after,
+		table.dataTable thead .sorting:before,
+		table.dataTable thead .sorting_asc:after,
+		table.dataTable thead .sorting_asc:before,
+		table.dataTable thead .sorting_asc_disabled:after,
+		table.dataTable thead .sorting_asc_disabled:before,
+		table.dataTable thead .sorting_desc:after,
+		table.dataTable thead .sorting_desc:before,
+		table.dataTable thead .sorting_desc_disabled:after,
+		table.dataTable thead .sorting_desc_disabled:before {
+		bottom: .5em;
+		}
+    </style>
 
-<h2>Gender : <?= $fetch_detail['gender']?></h2>
-<h2>Birthdate : <?= $fetch_detail['b_date']?></h2>
-<h2>Course & Year : <?= $fetch_detail['c_id']." - ".$fetch_detail['year']?></h2>
+    <title>S-Hub | <?= $fetch_detail['fname'].'-'.$fetch_detail['lname']?>'s Assignment</title>
+  </head>
+  <body>
 
-<hr>
+	<?php
+	include_once "nav_admin.php";
+	?>	
 
-<table  align="center" cellpadding="9" border="1">
-	<thead>
-		<tr>
-			<th>Date Uploaded</th>
-			<th>Filname</th>
-			<th>Other option</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-			$select = "select * from storage where s_id='$id' and s_name='$fname'";
-			$query = mysqli_query($con, $select);
-			while($fetch = mysqli_fetch_array($query)){
-		?>
-		<tr>
-			<td><?= $fetch['date']?></td>
-			<td><?= $fetch['filename']?></td>			
-			<td><button><a href="download.php?f_id=<?= $fetch['f_id']?>">Download</a></button></td>
-		</tr>
-		<?php } ?>
-	</tbody>
-</table>
+	<div class="container">
+	<div class="card p-4 mb-4">
+		<div class="row">
+		<div class="col" align="center">
+		<h4 class="card-title text-center"><?= $fetch_detail['fname'].'-'.$fetch_detail['lname']?></h4>
+					
+		<table class="table table-striped table-bordered table-sm">
+			<thead class="thead-dark">
+				<tr align="center">
+					<th class="th-sm">Stu Id</th>
+					<th class="th-sm">Firstame</th>
+					<th class="th-sm">Lastname</th>
+					<th class="th-sm">Gender</th>
+					<th class="th-sm">Birthdate</th>
+					<th class="th-sm">Course</th>
+					<th class="th-sm">Year</th>
+				</tr>
+			</thead>
+			<tbody >
+				<tr align="center">
+					<td><?= $fetch_detail['id']?></td>
+					<td><?= $fetch_detail['fname']?></td>
+					<td><?= $fetch_detail['lname']?></td>
+					<td><?= $fetch_detail['gender']?></td>
+					<td><?= $fetch_detail['b_date']?></td>
+					<td><?= $fetch_detail['c_id']?></td>
+					<td><?= $fetch_detail['year']?></td>
+				</tr>
+			</tbody>
+		</table>
+		</div>
+		</div>		
+	</div>	
+	</div>	
+
+
+<div class="container">
+	<div class="card p-4 mb-4">
+		<div class="row">
+		<div class="col" >
+		<h4 class="card-title text-center">Submitted Assignment</h4>
+			<table class="table  table-bordered table-sm  table-hover" align="center" cellpadding="9" border="1">
+				<thead class="thead-light" align="center">
+					<tr >
+						<th>#</th>
+						<th>Date Uploaded</th>
+						<th>Filename</th>
+						<th>Download</th>
+					</tr>
+				</thead>
+				<tbody >
+					<?php
+						$select = "select * from storage where s_id='$id' and s_name='$fname'";
+						$query = mysqli_query($con, $select);
+						$i =1; 
+						while($fetch = mysqli_fetch_array($query)){
+					?>
+					<tr align="center">
+						<td><?= $i++?></td>
+						<td><?= $fetch['date']?></td>
+						<td><?= $fetch['filename']?></td>			
+						<td><a href="download.php?f_id=<?= $fetch['f_id']?>"  class="btn btn-sm btn-mdb-color" ><i class="fas fa-download"></i></a></td>
+					</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+			</div>
+			</div>		
+		</div>	
+	</div>	
