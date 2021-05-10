1e6d923d8b09beb1abfd8e8f648d201e1b9f9ae7
Atanu <atanupal@live.com>
2021-05-10 16:39:19 +0530

    Added styling on add_material page

--- add_study_material.php
+++ add_study_material.php
@@ -1,33 +1,8 @@
 <?php
 	require 'session_validator.php';	
 	require_once 'conn.php';
-?>
-<h1>Add study material</h1>
-<hr>
-<form action="" method="post" enctype="multipart/form-data">
-	<input type="file" name="file" size="4" required="required"> <br>
-	<input type="text" name="title" placeholder="Enter title or topic name" required=""> <br>
-	Course : 
-	<select name="c_id" required="">
-		<option value="">select course</option>
-		<option value="B.Sc">B.Sc</option>
-		<option value="B.Ca">B.Ca</option>
-		<option value="B.Com">B.Com</option>
-		<option value="B.E">B.E</option>
-		<option value="B.Ba">B.Ba</option>
-		<option value="B.A">B.A</option>
-		<option value="M.Sc">M.Sc</option>
-		<option value="M.Ca">M.Ca</option>
-		<option value="M.Com">M.Com</option>
-		<option value="M.E">M.E</option>
-		<option value="M.Ba">M.Ba</option>
-		<option value="M.A">M.A</option>
-	</select><br>
-	<button name="save">Add File</button>
-</form>
-<?php // save material by admin 
-	
 
+	// save material by admin 
 	if(isset($_POST['save'])){	
 		
 		$c_id = $_POST['c_id'];						  	// course id
@@ -73,30 +48,137 @@
 		
 	}
 ?>
-<table align="center" cellpadding="9" border="1">
-	<thead>
-		<th>Date</th>
-		<th>Course</th>
-		<th>Title</th>
-		<th>File Name</th>
-		<th>Uploaded by</th>
-		<th>Option</th>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <?php include "stylesheet.php";?>
+    <style type="text/css">
+		table.dataTable thead .sorting:after,
+		table.dataTable thead .sorting:before,
+		table.dataTable thead .sorting_asc:after,
+		table.dataTable thead .sorting_asc:before,
+		table.dataTable thead .sorting_asc_disabled:after,
+		table.dataTable thead .sorting_asc_disabled:before,
+		table.dataTable thead .sorting_desc:after,
+		table.dataTable thead .sorting_desc:before,
+		table.dataTable thead .sorting_desc_disabled:after,
+		table.dataTable thead .sorting_desc_disabled:before {
+		bottom: .5em;
+		}
+    </style>
+
+    <title>S-Hub | Study Material</title>
+  </head>
+  <body>
+	
+	<?php
+		include_once "nav_admin.php";
+	?>
+
+<div class="container">
+	<div class="card p-4 mb-4">
+		<h5 class="card-title text-center">Add study material</h5>
+		<form action="" method="post" enctype="multipart/form-data">
+			<div class="form-row">
+				<div class="input-group input-group-sm col-md-4">
+					
+  						<div class="custom-file">
+    					<input type="file" name="file" class="custom-file-input" id="choosefile" required>
+    					<label class="custom-file-label" for="choosefile">Choose file</label>
+  						</div>
+  						
+					<!--div class="">
+					<label for="" class="sr-only">Upload File :</label>
+					<input type="file" name="file"  class="form-control form-control-sm " required="required">
+					</div-->
+				</div>
+				<div class="form-group col-md-4">
+					<label for="" class="sr-only">Enter title name :</label>
+					<input type="text" name="title" placeholder="Enter title or topic name" class="form-control " required=""> 
+				</div>
+				<div class="form-group col-auto">
+					<label for="" class="sr-only">Select course :</label>
+					<select name="c_id" class="form-control form-control-sm" required="">
+						<option value="">Select Course</option>
+						<option value="B.Sc">B.Sc</option>
+						<option value="B.Ca">B.Ca</option>
+						<option value="B.Com">B.Com</option>
+						<option value="B.E">B.E</option>
+						<option value="B.Ba">B.Ba</option>
+						<option value="B.A">B.A</option>
+						<option value="M.Sc">M.Sc</option>
+						<option value="M.Ca">M.Ca</option>
+						<option value="M.Com">M.Com</option>
+						<option value="M.E">M.E</option>
+						<option value="M.Ba">M.Ba</option>
+						<option value="M.A">M.A</option>
+					</select>
+					</div>
+					<div class="form-group col-auto">
+						<button name="save" class="btn btn-sm btn-mdb-color btn-block">Add File</button>
+					</div>
+				</div>
+			</div>
+		</form>
+</div>
+</div>
+
+<div class="container">
+	<div class="card p-4 mb-4">
+		<div class="row">
+		<div class="col" >
+		<h4 class="card-title text-center">Study Materials List</h4>
+<table id="material" class="table table-striped table-bordered table-sm" border="1">
+	<thead class="thead-dark" >
+		<tr align="center">
+			<th class="th-sm">Date</th>
+			<th class="th-sm">Course</th>
+			<th class="th-sm">Title</th>
+			<th class="th-sm">File Name</th>
+			<th class="th-sm">Uploaded by</th>
+			<th class="th-sm">Remove</th>
+		</tr>
 	</thead>
 	<tbody>
 		<?php
 			$query = mysqli_query($con, "select * from material");
 			while ($fetch = mysqli_fetch_array($query)) {
 		?>
-		<tr>
+		<tr align="center">
 			<td><?= $fetch['date']?></td>
 			<td><?= $fetch['c_id']?></td>
 			<td><?= $fetch['title']?></td>
 			<td><?= $fetch['name']?></td>
 			<td><?= $fetch['upload_by']?></td>
-			<td><button><a href="remove_study_material.php?id=<?= $fetch['id']?>" onclick="if(confirm('Are you sure you want to remove this study material ?')) return true; else return false;">Remove</a></button></td>
+			<td><a href="remove_study_material.php?id=<?= $fetch['id']?>" onclick="if(confirm('Are you sure you want to remove this study material ?')) return true; else return false;" class="btn btn-sm btn-danger"><i class="far fa-trash-alt"></i></a></td>
 		</tr>
 		<?php
 			}
 		?>
 	</tbody>
 </table>
+</div>
+</div>
+</div>
+</div>
+
+<?php include "script.php";?>
+<script type="text/javascript">
+	$(document).ready(function () {
+		$('#material').DataTable();
+		$('.dataTables_length').addClass('bs-select');
+
+		$('#adminNavbar .navbar-nav a').on('click', function(){
+			$('#adminNavbar .navbar-nav').find('li.active').removeClass('active');
+			$(this).parent('li').addClass('active');
+		});
+	});
+</script>
+
+</body>
+</html>
