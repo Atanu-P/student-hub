65e4cbfb8665d59db5c786b0ec38c2ec78950589
Atanu <atanupal@live.com>
2021-05-10 18:01:50 +0530

    Added styling on view page

--- view.php
+++ view.php
@@ -2,35 +2,88 @@
 	require 'session_validator.php';
 	require_once 'conn.php';
 ?>
-<h1>View assignment</h1><hr>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<table align="center"  border="1" cellpadding="9" >
-	<thead>
-		<tr>
-			<th>Date</th>
-			<th>Filename</th>
-			<th>Student Id</th>
-			<th>Student name</th>
-			<th>Course Name</th>
-			<th>Download</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-			$select = "select * from storage";
-			$query = mysqli_query($con, $select);
-			while ($fetch = mysqli_fetch_array($query)) {
-		?>
-		<tr>
-			<td><?= $fetch['date']?></td>
-			<td><?= $fetch['filename']?></td>
-			<td><?= $fetch['s_id']?></td>
-			<td><?= $fetch['s_name']?></td>
-			<td><?= $fetch['c_id']?></td>
-			<td> <button><a href="download.php?f_id=<?= $fetch['f_id']?>">Download</a></button> </td>
-		</tr>
-		<?php
-			}
-		?>
-	</tbody>
-</table>
+    <!-- Bootstrap CSS -->
+    <?php include "stylesheet.php";?>
+    <style type="text/css">
+		table.dataTable thead .sorting:after,
+		table.dataTable thead .sorting:before,
+		table.dataTable thead .sorting_asc:after,
+		table.dataTable thead .sorting_asc:before,
+		table.dataTable thead .sorting_asc_disabled:after,
+		table.dataTable thead .sorting_asc_disabled:before,
+		table.dataTable thead .sorting_desc:after,
+		table.dataTable thead .sorting_desc:before,
+		table.dataTable thead .sorting_desc_disabled:after,
+		table.dataTable thead .sorting_desc_disabled:before {
+		bottom: .5em;
+		}
+    </style>
+
+    <title>S-Hub | View Assignment</title>
+  </head>
+  <body>
+
+	<?php
+	include_once "nav_admin.php";
+	?>
+
+<div class="container">
+	<div class="card p-4 mb-4">
+	<div class="row">
+	<div class="col">
+		<h5 class="card-title text-center">View assignments submitted by student</h5>
+		<table id="viewAssignment"  class="table table-striped table-bordered table-sm" >
+			<thead class="thead-dark">
+				<tr align="center">
+					<th>Date</th>
+					<th>Student Id</th>
+					<th>Student name</th>
+					<th>Course Name</th>
+					<th>Filename</th>
+					<th>Download</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+					$select = "select * from storage";
+					$query = mysqli_query($con, $select);
+					while ($fetch = mysqli_fetch_array($query)) {
+				?>
+				<tr align="center">
+					<td><?= $fetch['date']?></td>
+					<td><?= $fetch['s_id']?></td>
+					<td><?= $fetch['s_name']?></td>
+					<td><?= $fetch['c_id']?></td>
+					<td><?= $fetch['filename']?></td>
+					<td> <a href="download.php?f_id=<?= $fetch['f_id']?>" class="btn btn-sm btn-grey"><i class="fas fa-download"></i></a> </td>
+				</tr>
+				<?php
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
+	</div>
+	</div>
+</div>
+<?php include "script.php";?>
+<script type="text/javascript">
+	$(document).ready(function () {
+		$('#viewAssignment').DataTable();
+		$('.dataTables_length').addClass('bs-select');
+
+		$('#adminNavbar .navbar-nav a').on('click', function(){
+			$('#adminNavbar .navbar-nav').find('li.active').removeClass('active');
+			$(this).parent('li').addClass('active');
+		});
+	});
+</script>
+</body>
+</html>
