6c55c931667afc2ce956024cc10f767871db36cf
Atanu <atanupal@live.com>
2021-05-13 16:21:31 +0530

    Added styling on v_call pages

--- v_call.php
+++ v_call.php
@@ -4,10 +4,37 @@
 
 	# admin videocall page
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <?php include "stylesheet.php";?>
+    
+
+    <title>S-Hub | Video Chat</title>
+  </head>
+  <body>
 	
+  	<?php
+	include_once "nav_admin.php";
+	?>
 
 
-<body>
-	<div id="otEmbedContainer" style="width:800px; height:640px"></div>
-                    <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=d4126eda-a9bd-493a-b09d-5734d8ca0d50&room=DEFAULT_ROOM"></script>
+	<div class="container mt-4">
+		<div class="card p-2 ">
+			<div class="row ">
+			<div class="col ">
+				<div id="otEmbedContainer" style="width:900px; height:600px" class="mx-auto"></div>
+            </div>
+     		</div>
+     	</div>
+ 	</div>
+
+<?php include "script.php";?>
+<script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=d4126eda-a9bd-493a-b09d-5734d8ca0d50&room=DEFAULT_ROOM"></script>
 </body>
+</html>
--- v_call_stu.php
+++ v_call_stu.php
@@ -11,7 +11,35 @@
 	require_once 'conn.php'; //db connection
 	# student videocall page
 ?>
-<body>
-	<div id="otEmbedContainer" style="width:800px; height:640px"></div>
-                    <script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=d4126eda-a9bd-493a-b09d-5734d8ca0d50&room=DEFAULT_ROOM"></script>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <?php include "stylesheet.php";?>
+    
+
+    <title>S-Hub | Video Chat</title>
+  </head>
+  <body>
+
+  	<?php include_once "nav_stu.php"; ?>
+
+  	<div class="container mt-4">
+		<div class="card p-2 ">
+			<div class="row ">
+			<div class="col ">
+				<div id="otEmbedContainer" style="width:900px; height:600px" class="mx-auto"></div>
+            </div>
+     		</div>
+     	</div>
+ 	</div>
+
+
+<?php include "script.php";?>
+<script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=d4126eda-a9bd-493a-b09d-5734d8ca0d50&room=DEFAULT_ROOM"></script>
 </body>
+</html>
